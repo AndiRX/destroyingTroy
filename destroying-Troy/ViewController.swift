4503ed4e86fc4a98b1d6b59c532911137bdd9f56
Petr <petr@petrs-air>
2017-10-27 20:52:28 +0200

    Initial Commit

---
+++ destroying-Troy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  destroying-Troy
+//
+//  Created by Petr on 27.10.17.
+//  Copyright © 2017 Andi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
